a6a91acd0bc03b4099cbb9e394eb3e3da9e1d249
kein3 <kevin@holiproject.com>
2025-06-03 09:59:57 +0200

    Use CDN as fallback with SRI

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,8 +11,12 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-    {{-- Tailwind via CDN --}}
-    <script src="https://cdn.tailwindcss.com"></script>
+    {{-- Tailwind via CDN if assets are not compiled --}}
+    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @else
+        <script src="https://cdn.tailwindcss.com" integrity="sha384-w8fXw5bAcRpC5Hcps225y7sY9qsK0kGugHgd6Nq35p3xNmPR9U1FVLtZLkYI7Di5yucs5cjox96D65V+1GPLRA==" crossorigin="anonymous"></script>
+    @endif
 </head>
 <body class="font-sans antialiased">
     <div class="min-h-screen bg-gray-100 flex">

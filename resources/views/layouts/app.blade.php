1a187cc319bbf1f90f5c7e938f16027b10f6e2df
kein3 <kevin@holiproject.com>
2025-06-03 10:10:41 +0200

    Restore Tailwind CDN with integrity

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,12 +11,8 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-99l6fv-codex/remplacer-le-script-cdn-dans-app.blade.php
-    {{-- Tailwind via CDN if assets are not compiled --}}
-    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    @else
-        <script src="https://cdn.tailwindcss.com" integrity="sha384-w8fXw5bAcRpC5Hcps225y7sY9qsK0kGugHgd6Nq35p3xNmPR9U1FVLtZLkYI7Di5yucs5cjox96D65V+1GPLRA==" crossorigin="anonymous"></script>
+    {{-- Tailwind via CDN --}}
+    <script src="https://cdn.tailwindcss.com" integrity="sha384-w8fXw5bAcRpC5Hcps225y7sY9qsK0kGugHgd6Nq35p3xNmPR9U1FVLtZLkYI7Di5yucs5cjox96D65V+1GPLRA==" crossorigin="anonymous"></script>
     @endif
 
     {{-- Compiled assets via Vite --}}
